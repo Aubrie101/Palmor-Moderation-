93c01201d9a55a1cccb97ecb8aab7d8d79f1fe61
BingBangBoom1212 <bwpt859g44@privaterelay.appleid.com>
2026-05-27 10:06:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Palmor moderation is online as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);

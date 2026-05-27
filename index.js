a6648f5114766a788cffa2ccceee3ec3c36e9870
BingBangBoom1212 <bwpt859g44@privaterelay.appleid.com>
2026-05-27 10:43:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,35 +17,50 @@ client.once('ready', () => {
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
-    // TEST COMMAND
     if (message.content === '!ping') {
         return message.reply('Pong!');
     }
 
-    // MODERATION: kick command
     if (message.content.startsWith('!kick')) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
             return message.reply("You don't have permission.");
         }
 
         const member = message.mentions.members.first();
-        if (!member) return message.reply("Mention someone to kick.");
+        if (!member) return message.reply('Mention someone to kick.');
 
-        await member.kick();
-        message.channel.send(`${member.user.tag} was kicked.`);
+        if (!member.kickable) {
+            return message.reply("I can't kick that user.");
+        }
+
+        try {
+            await member.kick();
+            return message.channel.send(`${member.user.tag} was kicked.`);
+        } catch (err) {
+            console.error(err);
+            return message.reply('Failed to kick that user.');
+        }
     }
 
-    // MODERATION: ban command
     if (message.content.startsWith('!ban')) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
             return message.reply("You don't have permission.");
         }
 
         const member = message.mentions.members.first();
-        if (!member) return message.reply("Mention someone to ban.");
+        if (!member) return message.reply('Mention someone to ban.');
 
-        await member.ban();
-        message.channel.send(`${member.user.tag} was banned.`);
+        if (!member.bannable) {
+            return message.reply("I can't ban that user.");
+        }
+
+        try {
+            await member.ban();
+            return message.channel.send(`${member.user.tag} was banned.`);
+        } catch (err) {
+            console.error(err);
+            return message.reply('Failed to ban that user.');
+        }
     }
 });
 

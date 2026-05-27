0b3c5f834ba8b460a80105cb1b909fc4c45bff71
BingBangBoom1212 <bwpt859g44@privaterelay.appleid.com>
2026-05-27 10:23:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
@@ -12,6 +12,41 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log(`Palmor moderation is online as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    // TEST COMMAND
+    if (message.content === '!ping') {
+        return message.reply('Pong!');
+    }
+
+    // MODERATION: kick command
+    if (message.content.startsWith('!kick')) {
+        if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+            return message.reply("You don't have permission.");
+        }
+
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Mention someone to kick.");
+
+        await member.kick();
+        message.channel.send(`${member.user.tag} was kicked.`);
+    }
+
+    // MODERATION: ban command
+    if (message.content.startsWith('!ban')) {
+        if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+            return message.reply("You don't have permission.");
+        }
+
+        const member = message.mentions.members.first();
+        if (!member) return message.reply("Mention someone to ban.");
+
+        await member.ban();
+        message.channel.send(`${member.user.tag} was banned.`);
+    }
 });
 
 client.login(process.env.TOKEN);
